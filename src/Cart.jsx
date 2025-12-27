2ff2313ad155bbc5694e0d8b3fd111bbcef7259e
Pooja <poojasri.s@skematix.in>
2025-12-27 20:18:13 +0530

    updates

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,16 +1,17 @@
-function CartComponant({ image,  category }) {
+function CartComponant({ image, category }) {
   return (
     <div className="rounded-xl overflow-hidden shadow-md border border-slate-700 hover:border-none hover:ring-2 hover:ring-amber-500 ">
       <img
         className="w-full h-48 object-contain bg-slate-500 p-4"
         src={image}
-       
       />
 
       <hr />
 
-      <div className="px-6 py-4 bg-slate-700 text-white text-center">
-        <p className="text-sm text-amber-400 font-bold uppercase">{category}</p>
+      <div className="px-6 py-4 bg-slate-700 hover:bg-amber-500 text-center  ">
+        <p className="text-sm text-amber-400  hover:text-slate-900 font-bold uppercase">
+          {category}
+        </p>
       </div>
     </div>
   );
